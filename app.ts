50525cad9de35877a2562bbbdf3732c945859f57
fatimaRiaz531 <162475613+fatimaRiaz531@users.noreply.github.com>
2024-03-06 20:56:45 +0500

    EXERCISE 2
    TYPESCRIPT ASSIGNMENT

---
+++ app.ts
@@ -0,0 +1,15 @@
+let personName: string='ERIC';
+console.log`hellow ${personName}would you like to learn some python today;?
+
+//exercise 3//
+
+//lowerCase//
+
+let personName: string = ''Fatima''
+console.log(''lower case;'', personName.tolowerCase()):
+
+//upper case//
+console.log("uppercase:", personName.tolocalUpperCase());
+
+//Title Case//
+console.log("titlecase;",personName.replace(/\b\w/g,c=> c.toUppercase()));
