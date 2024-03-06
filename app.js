84f5a4e7f21d19aa13def9b4fa3e81815caac94a
fatimaRiaz531 <162475613+fatimaRiaz531@users.noreply.github.com>
2024-03-06 20:54:11 +0500

    EXERCISE 2
    TYPESCRIPT ASSIGNMENT

---
+++ app.js
@@ -0,0 +1,6 @@
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+var personName = 'ERIC';
+console.log(__makeTemplateObject(["hellow ", "would you like to learn some python today;?\n\n//exercise 3//\n\n//lowerCase//\n\nlet personName: string = ''Fatima''\nconsole.log(''lower case;'', personName.tolowerCase()):\n\n//upper case//\nconsole.log(\"uppercase:\", personName.tolocalUpperCase());\n\n//Title Case//\nconsole.log(\"titlecase;\",personName.replace(/\bw/g,c=> c.toUppercase()));\n"], ["hellow ", "would you like to learn some python today;?\n\n//exercise 3//\n\n//lowerCase//\n\nlet personName: string = ''Fatima''\nconsole.log(''lower case;'', personName.tolowerCase()):\n\n//upper case//\nconsole.log(\"uppercase:\", personName.tolocalUpperCase());\n\n//Title Case//\nconsole.log(\"titlecase;\",personName.replace(/\\b\\w/g,c=> c.toUppercase()));\n"]), personName);
